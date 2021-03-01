559564923c0aab4a562878d1099680baa80dd982
ottogin <lukartoil@gmail.com>
2021-03-01 11:25:43 +0300

    NEW: helloworld, thirdparty linked

---
+++ sourcing/sourcing_script.cpp
@@ -0,0 +1,16 @@
+// MIT License
+// Copyright (c) [2021] [Artem Lukoianov]
+
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <iostream>
+
+#include <parquet/arrow/reader.h>
+
+int main(int argc, char** argv) {
+
+  std::cout << "Hello World" << std::endl;
+
+  return EXIT_SUCCESS;
+}
